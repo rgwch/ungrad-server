5c19f32aeaa59c7e0ba9514101a3b862822358ba
gerry <weirich@elexis.ch>
2016-09-04 19:28:41 +0200

    details

---
+++ dispatcher/src/test/java/Test_Restpoint.java
@@ -0,0 +1,16 @@
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by gerry on 04.09.16.
+ */
+@RunWith(VertxUnitRunner.class)
+public class Test_Restpoint {
+
+    @Test
+    public void launch(TestContext ctx){
+
+    }
+}
