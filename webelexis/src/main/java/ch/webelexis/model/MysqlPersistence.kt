1ec349ee4e35c468156a19ea0a9e3d1051958bbb
gerry <weirich@elexis.ch>
2016-10-24 08:40:27 +0200

    fix

--- webelexis/src/main/java/ch/webelexis/model/MysqlPersistence.kt
+++ webelexis/src/main/java/ch/webelexis/model/MysqlPersistence.kt
@@ -7,7 +7,7 @@ import io.vertx.core.AsyncResultHandler
  * Created by gerry on 22.10.2016.
  */
 class MysqlPersistence : Persistence{
-    override fun flush(collection: String, obj: AsyncPersistentObject, handler: (AsyncResult<Boolean>)->Unit) {
+    override fun flush(obj: AsyncPersistentObject, handler: (AsyncResult<Boolean>)->Unit) {
         throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
