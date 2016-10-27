1e8016132a0236a09abbce5678cbb74b80ad8d90
gerry <weirich@elexis.ch>
2016-10-27 20:43:37 +0200

    added lab classes

---
+++ webelexis/src/main/java/ch/webelexis/model/Laboratory.kt
@@ -0,0 +1,50 @@
+package ch.webelexis.model
+
+import io.vertx.core.Future
+
+/**
+ * Created by gerry on 27.10.16.
+ */
+
+class LabItem(id: String) : AsyncPersistentObject(id){
+    override val collection="LABORITEMS"
+    override val fieldnames=arrayOf(
+            Field("Title"),
+            Field("refMale","RefMann"),
+            Field("refFemale","RefFrauOrTx"),
+            Field("priority","prio"),
+            Field("group","Gruppe"),
+            Field("type","Typ"),
+            Field("unit","Einheit"),
+            Field("labID","LaborID"),
+            Field("shortname","Kuerzel"),
+            Field("formula"),
+            Field("digits"),
+            Field("visible"),
+            Field("billingcode"),
+            Field("loinc","loinccode")
+
+    )
+    override fun getLabel(): Future<String> {
+        return Future.future()
+    }
+
+}
+
+class LabResult(id:String): AsyncPersistentObject(id){
+    override val collection="LABORWERTE"
+    override val fieldnames=arrayOf(
+            Field("date","Datum"),
+            Field("time","Zeit"),
+            Field("flags","Flags"),
+            Field("comment","Kommentar"),
+            Field("result","Resultat"),
+            Field("itemID","ItemID"),
+            Field("patID","PatientID")
+    )
+
+    override fun getLabel(): Future<String> {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
