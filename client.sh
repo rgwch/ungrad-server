4e72c34b2e488e52ee206cf40bd8a277cc302d69
gerry <weirich@elexis.ch>
2016-11-02 20:38:39 +0100

    client build script

---
+++ client.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+rootdir=`pwd`
+console=${rootdir}/admin-ui
+dispatcher=${rootdir}/dispatcher
+
+cd ${console}
+rm -rf  ${dispatcher}/src/main/resources/web
+mkdir -p ${dispatcher}/src/main/resources/web/dist
+gulp export
+cp -r export/* ${dispatcher}/src/main/resources/web/
+cd ${rootdir}
