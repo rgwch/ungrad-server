72979b6a5ff95de46bcc51354858f9c15ff4f33b
gerry <weirich@elexis.ch>
2016-11-02 21:05:14 +0100

    all jars

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ PRODUCTS=${WEBSPACE}/products/ungrad-server
 mkdir -p ${PRODUCTS}/${BUILD_NUMBER}
 
 cp dispatcher/target/ungrad-server-dispatcher*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp webelexis/target/webelexis-*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp lucinda/target/lucinda-*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp tester/target/ungrad-server-tester-*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp -r article ${PRODUCTS}/${BUILD_NUMBER}
 
 rm ${PRODUCTS}/latest
 ln -s ${PRODUCTS}/${BUILD_NUMBER} ${PRODUCTS}/latest
