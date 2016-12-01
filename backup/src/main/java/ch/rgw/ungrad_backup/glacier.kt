131ec200f1c2431f994085a15346a4af79f0c033
gerry <weirich@elexis.ch>
2016-12-01 12:36:20 +0100

    start glacier backup

---
+++ backup/src/main/java/ch/rgw/ungrad_backup/glacier.kt
@@ -0,0 +1,12 @@
+package ch.rgw.ungrad_backup
+
+/**
+ * Created by gerry on 30.11.16.
+ */
+import com.amazonaws.ClientConfiguration
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+
+class Glacier{
+    val profileCredentialsProvider=ProfileCredentialsProvider()
+
+}
